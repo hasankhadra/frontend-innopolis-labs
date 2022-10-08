d09b873cca0c57012066f6bafa2cdb490723856c
hasankhadra <hasankhadra2013@gmail.com>
2022-10-08 19:14:14 +0000

    Deploying to gh-pages from @ hasankhadra/hasankhadra.github.io@a0edda0ed371e4e76bd247e9d9626fbd6d816a70 🚀

---
+++ _app/immutable/chunks/1-0b89148c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1d1ba6d0.js";export{t as component};
--- _app/immutable/chunks/4-b56c24d4.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-331790a0.js";import{default as t}from"../components/pages/randomFacts/_page.svelte-eaa96c51.js";export{t as component,r as shared};
