4c8dca8fd1fe9e4b4b0c91884a72cb6c307d3000
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2022-10-08 21:53:39 +0300

    fixed build path

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
 		}),
         paths: {
             // change below to your repo name
-            base: "/hasankhadra.github.io",
+            base: "",
         }
         // hydrate the <iv id="svelte"> element in src/app.html
     }
